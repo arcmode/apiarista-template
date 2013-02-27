35f80a101a9aeab5a663aa1cca759f0b539f9e45
David Rojas <drojascamaggi@gmail.com>
2013-02-26 22:43:10 -0300

    add placeholders to schema

--- data/schemas/item.js
+++ data/schemas/item.js
@@ -1,22 +1,13 @@
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema,
-	ObjectId = Schema.ObjectId,
-	User = require('../models/user');
+  Schema = mongoose.Schema;
+
+/*DEPENDENCIES PLACEHOLDER*/
 
 var #{Item}Schema = new Schema({
 
-	user: { type: ObjectId, ref: 'User', required: true },
+  /*FIELDS PLACEHOLDER*/
 
-	name: { type: String, required: true},
-	description: { type: String, required: true},
-
-	type: {
-	    type: String,
-	    required: true,
-	    lowercase: true,
-	    'enum': ['need', 'offer']},
-
-	date: { type: Date, default: Date.now() }
+  date: { type: Date, default: Date.now() }
 });
 
 module.exports = #{Item}Schema;
