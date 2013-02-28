9802e836f1ca0d2b99400b2957dab7bddb1d06de
David Rojas <drojascamaggi@gmail.com>
2013-02-27 21:59:06 -0300

    remove user and date from schema

--- data/schemas/resource.js
+++ data/schemas/resource.js
@@ -5,11 +5,7 @@ var mongoose = require('mongoose'),
 
 var ResourceSchema = new Schema({
 
-  user: { type: Schema.ObjectId,  ref: 'User' },
-
   //FIELDS PLACEHOLDER
-
-  date: { type: Date, default: Date.now() }
 });
 
 module.exports = ResourceSchema;
