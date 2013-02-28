82ba415a10118a01c7f43120c14a93108b6bc15b
David Rojas <drojascamaggi@gmail.com>
2013-02-28 15:13:31 -0300

    fix conditional

--- data/schemas/resource.js
+++ data/schemas/resource.js
@@ -7,7 +7,7 @@ var ResourceSchema = new Schema({
 
   //FIELDS PLACEHOLDER
 
-  //IF-OWNED , user: { type: Schema.ObjectId, ref: \'User\' }
+  //IF-OWNED , user: { type: Schema.ObjectId, ref: 'User' }
 
   //IF-TIMESTAMP , date: { type: Date, default: Date.now }
 });
