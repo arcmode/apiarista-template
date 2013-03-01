99c586f6cec6506655e7f136831b92f7e3359086
David Rojas <drojascamaggi@gmail.com>
2013-03-01 18:48:12 -0300

    always timestamp

--- data/schemas/resource.js
+++ data/schemas/resource.js
@@ -6,6 +6,8 @@ var mongoose = require('mongoose'),
 
 module.exports = new Schema({
 
+    timestamp: { type: Date, default: Date.now }
+
   //FIELDS PLACEHOLDER
 
   //IF_OWNER , owner: { Type: Schema.ObjectId, ref: '$Owner' }
@@ -15,6 +17,4 @@ module.exports = new Schema({
     //IF_AUTH default: require('../utils/tokenize'),
     //IF_AUTH unique: true,
     //IF_AUTH required: true }
-
-  //IF_TIMESTAMP , timestamp: { type: Date, default: Date.now }
 });
