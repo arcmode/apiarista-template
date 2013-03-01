9b8da3f4deac2762e580eceaad7cd56bcd0970d3
David Rojas <drojascamaggi@gmail.com>
2013-03-01 15:17:55 -0300

    change conditional convention

--- data/schemas/resource.js
+++ data/schemas/resource.js
@@ -8,13 +8,13 @@ module.exports = new Schema({
 
   //FIELDS PLACEHOLDER
 
-  //IF-OWNER , $owner: { Type: Schema.ObjectId, ref: '$Owner' }
+  //IF_OWNER , $owner: { Type: Schema.ObjectId, ref: '$Owner' }
 
-  //IF-AUTH , secret: { 
-    //IF-AUTH type: String,
-    //IF-AUTH default: require('../utils/tokenize'),
-    //IF-AUTH unique: true,
-    //IF-AUTH required: true }
+  //IF_AUTH , secret: { 
+    //IF_AUTH type: String,
+    //IF_AUTH default: require('../utils/tokenize'),
+    //IF_AUTH unique: true,
+    //IF_AUTH required: true }
 
-  //IF-TIMESTAMP , $timestamp: { type: Date, default: Date.now }
+  //IF_TIMESTAMP , $timestamp: { type: Date, default: Date.now }
 });
--- routes/resources/resource/index.js
+++ routes/resources/resource/index.js
@@ -3,9 +3,9 @@ var load = require('../../middleware/load')('$resource'),
     list = require('./crud/list'),
     update = require('./crud/update'),
     _delete = require('./crud/delete'),
-    //IF-OWNER load$Owner = require('../../middleware/load')('$owner'),
-    //IF-OWNER auth$OwnerOwnership = require('../../middleware/ownership')('$resource', '$owner'),
-    //IF-OWNER auth$Owner = require('../../middleware/auth')('$owner'),
+    //IF_OWNER load$Owner = require('../../middleware/load')('$owner'),
+    //IF_OWNER auth$OwnerOwnership = require('../../middleware/ownership')('$resource', '$owner'),
+    //IF_OWNER auth$Owner = require('../../middleware/auth')('$owner'),
     //IF_AUTH auth$Resource = require('../../middleware/auth')('$resource'),
     //IF_AUTH same = require('../../middleware/same')('$resource'),
     send = require('../../middleware/send')('$resource');
@@ -16,38 +16,38 @@ module.exports = function(app) {
 
     app.all('/:$resource_id', load);
 
-    //IF-AUTH app.all('/:$resource_id', auth$Resource);
+    //IF_AUTH app.all('/:$resource_id', auth$Resource);
 
     app
       .get('/',
-              //IF-OWNER //IF-PPRIVATE load$Owner,
-              //IF-OWNER //IF-PPRIVATE auth$Owner,
+              //IF_OWNER //IF_PPRIVATE load$Owner,
+              //IF_OWNER //IF_PPRIVATE auth$Owner,
               list,
               send)
 
       .post('/',
-                //IF-OWNER load$Owner,
-                //IF-OWNER auth$Owner,
+                //IF_OWNER load$Owner,
+                //IF_OWNER auth$Owner,
                 create,
                 send)
 
       .get('/:$resource_id',
-                            //IF-OWNER //IF-PPRIVATE load$Owner,
-                            //IF-OWNER //IF-PPRIVATE auth$Owner,
+                            //IF_OWNER //IF_PPRIVATE load$Owner,
+                            //IF_OWNER //IF_PPRIVATE auth$Owner,
                             send)
 
       .put('/:$resource_id',
-                            //IF-OWNER load$Owner,
-                            //IF-OWNER auth$Owner,
-                            //IF-OWNER auth$OwnerOwnership,
+                            //IF_OWNER load$Owner,
+                            //IF_OWNER auth$Owner,
+                            //IF_OWNER auth$OwnerOwnership,
                             //IF_AUTH same},
                             update,
                             send)
 
       .del('/:$resource_id',
-                            //IF-OWNER load$Owner,
-                            //IF-OWNER auth$Owner,
-                            //IF-OWNER auth$OwnerOwnership,
+                            //IF_OWNER load$Owner,
+                            //IF_OWNER auth$Owner,
+                            //IF_OWNER auth$OwnerOwnership,
                             //IF_AUTH same,
                             _delete,
                             send);
